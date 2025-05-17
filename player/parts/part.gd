9a1df10597c74e4c0cbe4be28add79d56d35a03b
Viktor Mitts <vikezor@disroot.org>
2025-05-17 15:24:15 +0300

    add a comment :)

--- player/parts/part.gd
+++ player/parts/part.gd
@@ -1,4 +1,6 @@
 class_name Part extends RigidBody2D
 
+# this would probably be a hell of a lot more efficient as a const,
+# but subclasses can't override those
 func get_part_name():
 	return "Part"

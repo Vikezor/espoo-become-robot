a869b48ead049b7da1a03a79958ec867f977ea3c
Viktor Mitts <vikezor@disroot.org>
2025-05-17 19:52:09 +0300

    fix static not attaching to things, but now spinners don't spin when attached to statics

--- player/parts/static.gd
+++ player/parts/static.gd
@@ -1,9 +1,19 @@
 class_name Static extends Part
 
 
+var initial_transform: Transform2D
+
+
 #func set_attachment(node: PhysicsBody2D):
 	#$PinJoint2D.node_a = get_path_to(node)
 
 
 func get_part_name():
 	return "Static"
+
+
+func _process(delta):
+	if not freeze:
+		transform = initial_transform
+	else:
+		initial_transform = transform

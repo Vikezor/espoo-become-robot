64cbe16d22f009ed9e8adbfc5320015fe1ab0f2b
Viktor Mitts <vikezor@disroot.org>
2025-05-17 15:37:54 +0300

    freeze the instantiated part in the menu

--- player/editor/parts.gd
+++ player/editor/parts.gd
@@ -2,9 +2,16 @@ extends VBoxContainer
 
 const entry_scene = preload("res://player/editor/part_entry.tscn")
 
+signal part_clicked(part: Part, event: InputEvent)
+
 func _ready():
 	for part in ResourceLoader.list_directory("res://player/parts/scenes"):
-		var loaded = load("res://player/parts/scenes/" + part).instantiate()
+		var loaded: Part = load("res://player/parts/scenes/" + part).instantiate()
+		loaded.freeze = true
 		var entry: PartEntry = entry_scene.instantiate()
 		entry.part = loaded
 		add_child(entry)
+		entry.clicked.connect(_on_clicked)
+
+func _on_clicked(part: Part, event: InputEvent):
+	emit_signal("part_clicked", part, event)

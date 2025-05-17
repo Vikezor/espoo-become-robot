717c455c71c4772fe589f263ba5e771dab76e77a
Viktor Mitts <vikezor@disroot.org>
2025-05-17 17:37:31 +0300

    add a Go button that starts the level with the guy you've built

--- player/editor/editor.gd
+++ player/editor/editor.gd
@@ -3,11 +3,13 @@ extends Control
 
 var spawned_part: Part
 var clicking := false
+const level_scene = preload("res://test/testlevel.tscn")
+@onready var player = $CenterContainer/Container/Player
 
 func _on_parts_part_clicked(part):
 	spawned_part = part.duplicate()
-	spawned_part.player = $CenterContainer/Container/Player
-	$CenterContainer/Container/Player.add_child(spawned_part)
+	spawned_part.set_player(player)
+	player.add_child(spawned_part)
 
 
 func _process(_delta):
@@ -18,3 +20,14 @@ func _process(_delta):
 func _on_gui_input(event):
 	if spawned_part != null and event is InputEventMouseButton and not event.pressed:
 		spawned_part = null
+
+
+func _on_go_pressed():
+	var level = level_scene.instantiate()
+	var spawnpos = level.get_child(0)
+	$CenterContainer/Container.remove_child(player)
+	level.add_child(player)
+	player.position = spawnpos.position
+	player.freeze = false
+	get_tree().root.add_child(level)
+	get_tree().root.remove_child(self)

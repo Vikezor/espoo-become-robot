36a51a24d9ee4a8ef0de433d6c36d1a393f8e6fd
Viktor Mitts <vikezor@disroot.org>
2025-05-17 16:56:50 +0300

    make the spawned part follow the cursor

--- player/editor/editor.gd
+++ player/editor/editor.gd
@@ -1,8 +1,19 @@
 extends Control
 
 
+var spawned_part: Part
+var clicking := false
+
 func _on_parts_part_clicked(part):
-	var spawned_part: Part = part.duplicate()
+	spawned_part = part.duplicate()
 	$CenterContainer/Container/Player.add_child(spawned_part)
-	while Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		spawned_part.position = get_local_mouse_position()
+
+
+func _process(_delta):
+	if spawned_part != null:
+		spawned_part.position = get_local_mouse_position() - $CenterContainer/Container.position
+
+
+func _on_gui_input(event):
+	if spawned_part != null and event is InputEventMouseButton and not event.pressed:
+		spawned_part = null
